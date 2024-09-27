d4813e0bb636101a32248fa4aac92bf875ee9679
aibeknet <aaskarov0690.kg@gmail.com>
2024-09-27 18:31:06 +0600

    Added the types.d.ts file in the src directory.Created the "Ingredient" type. And also added images to assets. And then he created an array of ingredients.

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+import meatImage from './assets/meat.png';
+import cheeseImage from './assets/cheese.png';
+import saladImage from './assets/salad.png';
+import baconImage from './assets/bacon.png';
+export interface Ingredient {
+    name: string;
+    price: number;
+    image: string;
+    count: number;
+}
+
+export const INGREDIENTS: Ingredient[] = [
+    { name: 'Meat', price: 80, image: meatImage, count: 0},
+    { name: 'Cheese', price: 50, image: cheeseImage, count: 0},
+    { name: 'Salad', price: 10, image: saladImage, count: 0},
+    { name: 'Bacon', price: 60, image: baconImage, count: 0},
+];
